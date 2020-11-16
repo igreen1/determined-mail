45d5929765232b9a7492601e768521cf6d6ad141
Halle Vogelpohl <hvogelpo@lion.lmu.edu>
2020-11-16 13:09:49 -0800

    Fixed issue #14. Updated CSS for newMessage.js and added "Save Draft" and "Send" buttons.

--- src/components/NewMessage.js
+++ src/components/NewMessage.js
@@ -4,59 +4,63 @@ import PropTypes from "prop-types";
 
 const inputStyle = {
   backgroundColor: "#fff",
-  width: "98.6%",
-  height: "5vh",
+  height: "3vh",
+  borderRadius: "0px 0px 4px 4px",
   display: "inline-block",
   border: "5px solid",
   borderColor: "#fb6949",
-  borderRadius: "0px 0px 4px 4px ",
   fontFamily: "arial",
   fontSize: "25px",
   color: "#000",
-  margin: "0px 0px 0px 5px",
-};
-
-const toLabelStyle = {
-  backgroundColor: "#fb6949",
-  width: "98.35%",
-  height: "3vh",
-  display: "inline-block",
-  borderRadius: "4px 4px 0px 0px",
-  fontFamily: "arial",
-  fontSize: "25px",
-  color: "#fff",
-  padding: "20px 0px 5px 20px",
-  border: "none",
-  margin: "30px 0px 0px 5px",
 };
 
 const labelStyle = {
   backgroundColor: "#fb6949",
-  width: "98.35%",
   height: "3vh",
-  display: "inline-block",
   borderRadius: "4px 4px 0px 0px",
   fontFamily: "arial",
   fontSize: "25px",
   color: "#fff",
-  padding: "20px 0px 5px 20px",
-  border: "none",
-  margin: "20px 0px 0px 5px",
+  padding: "10px 0px 5px 10px",
+  marginTop: "10px",
 };
 
 const draftEmailStyle = {
+  resize: "none",
   backgroundColor: "#fff",
-  width: "98.5%",
   height: "50vh",
   display: "inline-block",
   borderRadius: "0px 0px 10px 10px",
   fontFamily: "arial",
   fontSize: "25px",
-  padding: "none",
   border: "5px solid",
   borderColor: "#fb6949",
   color: "#000",
-  margin: "0px 0px 0px 5px",
+};
+
+const buttonStyle = {
+  backgroundColor: "#fb6949",
+  borderRadius: "4px",
+  color: "#fff",
+  fontSize: "25px",
+  width: "10vw",
+  height: "5vh",
+  align: "center",
+  margin: "5px",
+  border: "none",
+};
+
+const buttonContainer = {
+  display: "flex",
+  flexDirection: "row-reverse",
+  width: "80vw",
+  marginTop: "5px",
+};
+
+const containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  width: "80vw",
 };
 
 class NewMesssage extends Component {
@@ -64,47 +68,63 @@ class NewMesssage extends Component {
     return (
       <div>
         <header className="page-header">New Message</header>
-        <label class="label" style={toLabelStyle}>
-          <b>To:</b>
-        </label>
-        <input
-          type="text"
-          className="To"
-          singleLine="true"
-          title="To"
-          style={inputStyle}
-          placeholder="Enter your email recipient here"
-        />
-        <label class="label" style={labelStyle}>
-          <b>CC:</b>
-        </label>
-        <input
-          type="text"
-          className="To"
-          singleLine="true"
-          style={inputStyle}
-          placeholder="Enter your CC recipient here"
-        />
-        <label class="label" style={labelStyle}>
-          <b>BCC:</b>
-        </label>
-        <input
-          type="text"
-          className="To"
-          singleLine="true"
-          style={inputStyle}
-          placeholder="Enter your BCC recipient here"
-        />
-        <label class="label" style={labelStyle}>
-          <b>Draft Email</b>
-        </label>
-        <textarea
-          type="text"
-          className="To"
-          singleLine="true"
-          style={draftEmailStyle}
-          placeholder="Draft your email here"
-        />
+        <div class="container" style={containerStyle}>
+          <label class="label" style={labelStyle}>
+            <b>To:</b>
+          </label>
+          <input
+            type="text"
+            className="To"
+            singleLine="true"
+            title="To"
+            style={inputStyle}
+            placeholder="Enter your email recipient here"
+          />
+        </div>
+        <div class="container" style={containerStyle}>
+          <label class="label" style={labelStyle}>
+            <b>CC:</b>
+          </label>
+          <input
+            type="text"
+            className="To"
+            singleLine="true"
+            style={inputStyle}
+            placeholder="Enter your CC recipient here"
+          />
+        </div>
+        <div class="container" style={containerStyle}>
+          <label class="label" style={labelStyle}>
+            <b>BCC:</b>
+          </label>
+          <input
+            type="text"
+            className="To"
+            singleLine="true"
+            style={inputStyle}
+            placeholder="Enter your BCC recipient here"
+          />
+        </div>
+        <div class="container" style={containerStyle}>
+          <label class="label" style={labelStyle}>
+            <b>Draft Email</b>
+          </label>
+          <textarea
+            type="text"
+            className="To"
+            singleLine="true"
+            style={draftEmailStyle}
+            placeholder="Draft your email here"
+          />
+        </div>
+        <div class="buttonContainer" style={buttonContainer}>
+          <button class="button" style={buttonStyle}>
+            Send
+          </button>
+          <button class="button" style={buttonStyle}>
+            Save Draft
+          </button>
+        </div>
       </div>
     );
   }
