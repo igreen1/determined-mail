7edc7f1e172fbfae2627b53dfaad9e4dad3a3c4a
igreen1 <iangreen2.00@gmail.com>
2020-11-22 13:11:02 -0800

    Forgot to commit this, sorry

---
+++ src/useEmails.js
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+//spam, delete, draft, add in general,
+export default (initialValue) => {
+  const [emails, setEmails] = useState(initialValue)
+
+  return {
+    emails,
+    setEmails,
+
+    grabPage: (page) => {
+        return emails.filter((email)=> email.page === page)
+    },
+
+    deleteEmail: (key) => {
+      setEmails(emails.filter((email) => email.id !== key))
+    },
+
+    selectEmail: (key) => {
+      setEmails(
+        emails.map((email) => {
+          if (email.id === key) {
+            email.selected = !email.selected
+          }
+        })
+      )
+    },
+
+    selectedToSpam: () => {
+      setEmails(
+        emails.map((email) => {
+          if (email.selected) {
+            email.page = 'spam'
+          }
+        })
+      )
+    },
+
+    selectedToDeleted: () => {
+      setEmails(
+        emails.map((email) => {
+          if (email.selected) {
+            email.page = 'deleted'
+          }
+        })
+      )
+    },
+
+    selectedToInbox: () => {
+      setEmails(
+        emails.map((email) => {
+          if (email.selected) {
+            email.page = 'inbox'
+          }
+        })
+      )
+    },
+
+    saveDraft: (newEmail) => {
+      let draftEmail = { ...newEmail }
+      draftEmail.selected = false
+      draftEmail.page = 'draft'
+      draftEmail.id = Date.now()
+      setEmails([...emails, draftEmail])
+    },
+  }
+}
