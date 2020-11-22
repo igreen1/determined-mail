322fc5813a3cee3dd6ae1777935e04d81772afc7
igreen1 <iangreen2.00@gmail.com>
2020-11-21 16:50:32 -0800

    Added multi-page support (only render of one-page) And added sort-by-id feature to emaillist object

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import EmailItem from './EmailItem'
 
-const EmailList = ({ emails, deleteEmail, selectEmail}) => {
-  return emails.sort((email) => email.id).map((email) => (
+const EmailList = ({ page, emails, deleteEmail, selectEmail}) => {
+  return emails.sort((a,b) => a.id - b.id)
+  .filter( (email) => email.page === page)
+  .map((email) => (
     <EmailItem key={email.id} email={email} deleteEmail={deleteEmail} selectEmail={selectEmail} />
   ))
 }
