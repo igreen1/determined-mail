b198eb24422c090c7fe8c754525640fec926654d
Halle Vogelpohl <hvogelpo@lion.lmu.edu>
2020-11-23 13:33:40 -0800

    Fixed CSS

--- src/components/WriteEmail.js
+++ src/components/WriteEmail.js
@@ -1,32 +1,42 @@
 /* eslint-disable */
-import React, {useState} from "react"
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
 import { useHistory } from 'react-router-dom'
-
+import './WriteEmail.css'
 /* eslint-disable */
 
-const WriteEmail = ({sendEmail, saveDraft, emails}) => {
-
+const WriteEmail = ({ sendEmail, saveDraft, emails }) => {
   const history = useHistory()
 
   let email
-  if(emails){
-    const {id} = useParams();
-    [email] = emails.filter((email) => email.id === Number(id))
+  if (emails) {
+    const { id } = useParams()
+    ;[email] = emails.filter((email) => email.id === Number(id))
   }
-  //make custom hook :) 
-  const [to, setTo] = email ? useState(email.to) : useState("")
-  const [cc, setCC] = email ? useState(email.cc) : useState("")
-  const [bcc, setBCC] = email ? useState(email.bcc) : useState("")
-  const [body, setBody] = email ? useState(email.body) : useState("")
-  const [subject, setSubject] = email ? useState(email.subject) : useState("")
+  //make custom hook :)
+  const [to, setTo] = email ? useState(email.to) : useState('')
+  const [cc, setCC] = email ? useState(email.cc) : useState('')
+  const [bcc, setBCC] = email ? useState(email.bcc) : useState('')
+  const [body, setBody] = email ? useState(email.body) : useState('')
+  const [subject, setSubject] = email ? useState(email.subject) : useState('')
 
-
-  return( 
-      <div>
-      <div className="textfieldContainer" >
-        <label className="label" >
+  return (
+    <div>
+      <div className="textfieldContainer">
+        <label className="label">
+          <b>To:</b>
+        </label>
+        <input
+          type="text"
+          className="To"
+          value={to}
+          placeholder="Enter your email recipient here"
+          onChange={(event) => setCC(event.target.value)}
+        />
+      </div>
+      <div className="textfieldContainer">
+        <label className="label">
           <b>CC:</b>
         </label>
         <input
@@ -34,23 +44,23 @@ const WriteEmail = ({sendEmail, saveDraft, emails}) => {
           className="CC"
           value={cc}
           placeholder="Enter your CC recipient here"
-          onChange={event => setCC(event.target.value)}
+          onChange={(event) => setCC(event.target.value)}
         />
       </div>
       <div className="textfieldContainer">
-        <label className="label" >
+        <label className="label">
           <b>BCC:</b>
         </label>
         <input
           type="text"
           className="BCC"
           value={bcc}
-          onChange={event=>setBCC(event.target.value)}
+          onChange={(event) => setBCC(event.target.value)}
           placeholder="Enter your BCC recipient here"
         />
       </div>
-      <div className="textfieldContainer" >
-        <label className="label" >
+      <div className="textfieldContainer">
+        <label className="label">
           <b>Subject:</b>
         </label>
         <input
@@ -59,54 +69,55 @@ const WriteEmail = ({sendEmail, saveDraft, emails}) => {
           title="Subject"
           value={subject}
           placeholder="Enter subject of email here"
-          onChange={event=> setSubject(event.target.value)}
+          onChange={(event) => setSubject(event.target.value)}
         />
       </div>
       <div className="textfieldContainer">
-        <label className="label" >
+        <label className="label">
           <b>Draft Email</b>
         </label>
         <textarea
           type="text"
-          className="body"
+          className="draftEmail"
           value={body}
           placeholder="Draft your email here"
-          onChange={event=>setBody(event.target.value)}
+          onChange={(event) => setBody(event.target.value)}
         />
       </div>
       <div className="buttonContainer">
-        <button className="button" onClick = {() => sendEmail( { subject, to, bcc, cc, body } )} >
+        <button
+          className="button"
+          onClick={() => sendEmail({ subject, to, bcc, cc, body })}
+        >
           Send
         </button>
-        <button 
-          className="button" 
-          onClick = {() => {
+        <button
+          className="button"
+          onClick={() => {
             const id = email?.id
-            console.log(id);
-            saveDraft( { subject, to, bcc, cc, body, id} )
+            console.log(id)
+            saveDraft({ subject, to, bcc, cc, body, id })
             history.push('/drafts')
-          }} 
-          >
+          }}
+        >
           Save Draft
         </button>
       </div>
     </div>
   )
-
 }
-
 
 WriteEmail.propTypes = {
-    sendEmail: PropTypes.func.isRequired,
-    saveDraft: PropTypes.func.isRequired,
-    email: PropTypes.shape({
-        to: PropTypes.string,
-        cc:PropTypes.string,
-        bcc:PropTypes.string,
-        subject:PropTypes.string,
-        body:PropTypes.string,
-    }), //optional
-    emails: PropTypes.array,
+  sendEmail: PropTypes.func.isRequired,
+  saveDraft: PropTypes.func.isRequired,
+  email: PropTypes.shape({
+    to: PropTypes.string,
+    cc: PropTypes.string,
+    bcc: PropTypes.string,
+    subject: PropTypes.string,
+    body: PropTypes.string,
+  }), //optional
+  emails: PropTypes.array,
 }
 
-export default WriteEmail;
+export default WriteEmail
