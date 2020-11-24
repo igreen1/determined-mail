e171f2b1204b0ca67a77d3867a07f182187ebf27
Sophia <sophia.lisette@gmail.com>
2020-11-23 16:45:37 -0800

    Updating branch with changes in main

--- src/components/EmailItem.js
+++ src/components/EmailItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BsFillTrashFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
+/* eslint-disable */
 
 const EmailItem = ({ email, deleteEmail, selectEmail }) => {
   let emailInfo
